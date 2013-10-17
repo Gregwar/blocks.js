d0e2d794b915638666efee019c2386c40fa6ce2b
Gregwar <g.passault@gmail.com>
2013-10-17 18:10:05 +0200

    Segment: prototype

--- js/segment.js
+++ js/segment.js
@@ -4,68 +4,68 @@ function Segment(x, y, dx, dy)
     this.y = y;
     this.dx = dx;
     this.dy = dy;
+};
 
-    /**
-     * Distance
-     */
-    this.distance = function(point1, point2)
-    {
-        return Math.sqrt(Math.pow(point2.x-point1.x,2) + Math.pow(point2.y-point1.y,2));
+/**
+ * Distance
+ */
+Segment.prototype.distance = function(point1, point2)
+{
+    return Math.sqrt(Math.pow(point2.x-point1.x,2) + Math.pow(point2.y-point1.y,2));
+}
+
+/**
+ * Distance with a point
+ */
+Segment.prototype.distanceP = function(point)
+{
+    var normal = this.normal();
+    normal.x = point.x;
+    normal.y = point.y;
+    var intersection = this.intersection(normal);
+
+    return [intersection[0], this.distance(normal.alpha(intersection[1]), point)];
+};
+
+/**
+ * Normal
+ */
+Segment.prototype.normal = function()
+{
+    return new Segment(this.x, this.y, this.dy, -this.dx);
+};
+
+/**
+ * Gets the intersection alpha with another 
+ */
+Segment.prototype.intersection = function(other)
+{
+    var a = this.dx;
+    var b = -other.dx;
+    var c = this.dy;
+    var d = -other.dy;
+    var b0 = other.x-this.x;
+    var b1 = other.y-this.y;
+    var det = a*d - b*c;
+
+    if (det == 0) {
+        return null;
     }
 
-    /**
-     * Distance with a point
-     */
-    this.distanceP = function(point)
-    {
-        var normal = this.normal();
-        normal.x = point.x;
-        normal.y = point.y;
-        var intersection = this.intersection(normal);
+    r1 = (d*b0 - b*b1)/det;
+    r2 = (-c*b0 + a*b1)/det;
 
-        return [intersection[0], this.distance(normal.alpha(intersection[1]), point)];
-    };
+    return [r1, r2];
+};
 
-    /**
-     * Normal
-     */
-    this.normal = function()
-    {
-        return new Segment(this.x, this.y, this.dy, -this.dx);
-    };
+/**
+ * Gets the alpha point
+ */
+Segment.prototype.alpha = function(a)
+{
+    var point = {};
+    point.x = this.x+this.dx*a;
+    point.y = this.y+this.dy*a;
 
-    /**
-     * Gets the intersection alpha with another 
-     */
-    this.intersection = function(other)
-    {
-        var a = this.dx;
-        var b = -other.dx;
-        var c = this.dy;
-        var d = -other.dy;
-        var b0 = other.x-this.x;
-        var b1 = other.y-this.y;
-        var det = a*d - b*c;
-
-        if (det == 0) {
-            return null;
-        }
-
-        r1 = (d*b0 - b*b1)/det;
-        r2 = (-c*b0 + a*b1)/det;
-
-        return [r1, r2];
-    };
-
-    /**
-     * Gets the alpha point
-     */
-    this.alpha = function(a)
-    {
-        var point = {};
-        point.x = this.x+this.dx*a;
-        point.y = this.y+this.dy*a;
-
-        return point;
-    };
+    return point;
 };
